c816ea39ca81503c56385c912eb2771401fcfc01
Yoandre Saavedra <yoandresaavedra@gmail.com>
2019-07-20 16:43:28 -0500

    initial commit

---
+++ web/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.utils.translation import gettext_lazy as _
+
+from .views import (
+    HomeView, ToursView, AboutView, GalleryView,
+    BlogView, ContactsView
+)
+
+app_name = 'web'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path(_('tours/'), ToursView.as_view(), name='tours'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('gallery/', GalleryView.as_view(), name='gallery'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+]
